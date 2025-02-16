7db97cff41683cace815624b83f66222ef6bbe9a
k. <kehiiiiya@gmail.com>
2025-02-16 20:18:11 +0000

    Update config.go

--- zapoli/config.go
+++ zapoli/config.go
@@ -24,22 +24,12 @@ type Config struct {
 	BlossomPort string
 }
 
-func LoadConfig() Config {
-	godotenv.Load(".env")
-
-	if os.Getenv("RELAY_ICON") == "" {
-		os.Setenv("RELAY_ICON", "https://file.nostrmedia.com/f/bd4ae3e67e29964d494172261dc45395c89f6bd2e774642e366127171dfb81f5/634c632c4672e4d7e3cc94f0789f440842d1f435d92277aa2e233cfd0055ffa7.png")
+func LoadConfig() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Can't load .env: %s\n", err.Error())
 	}
 
-	if os.Getenv("RELAY_BANNER") == "" {
-		os.Setenv("RELAY_BANNER", "https://file.nostrmedia.com/f/bd4ae3e67e29964d494172261dc45395c89f6bd2e774642e366127171dfb81f5/0255481dd1eaa1632928a9258e1faa727eeaf45f3b6d8b8ec0a959e13d092298.png")
-	}
-
-	if os.Getenv("RELAY_CONTACT") == "" {
-		os.Setenv("RELAY_CONTACT", getEnv("RELAY_PUBKEY"))
-	}
-
-	config := Config{
+	config = Config{
 		RelayName:        getEnv("RELAY_NAME"),
 		RelayPubkey:      getEnv("RELAY_PUBKEY"),
 		RelayDescription: getEnv("RELAY_DESCRIPTION"),
@@ -55,8 +45,6 @@ func LoadConfig() Config {
 		RelayPort:   getEnv("RELAY_PORT"),
 		BlossomPort: getEnv("BLOSSOM_PORT"),
 	}
-
-	return config
 }
 
 func getEnv(key string) string {
